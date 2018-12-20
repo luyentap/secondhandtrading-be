280e44f1a555549904eb59b0ada7f100322c6b6f
gaga201702 <bamboo2017002@gmail.com>
2018-12-20 13:10:54 +0700

    fix redirect login

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,7 +26,7 @@ var auth = {
         // res.send({user, token});
         // res.redirect('/login?token='+ token);
         res.writeHead(301, {
-            location: "http://secondhandtrading-be.herokuapp.com/" + "success_login?token=" + token
+            location: "http://secondhandtrading-fe.herokuapp.com/" + "success_login?token=" + token
         });
         res.end();
     },
